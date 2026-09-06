f124f0717c8ae87bf922f9ecb893fcd3661692d2
lyman.lu <luyi980521@gmail.com>
2026-09-06 19:56:23 +0900

    feat: fix: resolve application startup issues

--- asteria-bootstrap/src/main/java/io/asteria/bootstrap/AsteriaApplication.java
+++ asteria-bootstrap/src/main/java/io/asteria/bootstrap/AsteriaApplication.java
@@ -1,11 +1,13 @@
 package io.asteria.bootstrap;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
+//@EnableScheduling
+@MapperScan(value = "io.asteria", annotationClass = Mapper.class)
 @SpringBootApplication(scanBasePackages = "io.asteria")
-@EnableScheduling
 public class AsteriaApplication {
 
     public static void main(String[] args) {
